41f753bf841582089020b4db45d9e561656de271
manacacia@gmail.com <manacacia@gmail.com@1bba4816-cec9-a7a2-e19b-818e2af6ccd6>
2012-11-10 21:13:28 +0000

    Added AcaciaAnn.jar to combine.bat

--- jar/combine.bat
+++ jar/combine.bat
@@ -18,10 +18,12 @@ rem You should have received a copy of the GNU General Public License
 rem along with acacia-lex.  If not, see <http://www.gnu.org/licenses/>.
 rem 
 
+xcopy /y "C:\Darbs\Acacia\Source\AcaciaAnn\dist\AcaciaAnn.jar" "C:\Darbs\AcaciaAnn\jar\AcaciaAnn.jar"
 xcopy /y "C:\Darbs\Acacia\Source\Acacia\dist\Acacia.jar" "C:\Darbs\Acacia\jar\Acacia.jar"
 xcopy /y ""C:\Darbs\Acacia\Source\AcaciaLex\dist\AcaciaLex.jar"" "C:\Darbs\Acacia\jar\AcaciaLex.jar"
 
 cd tmp
+C:\Java\jdk1.6.0_12\bin\jar -xf ../AcaciaAnn.jar
 C:\Java\jdk1.6.0_12\bin\jar -xf ../Acacia.jar
 C:\Java\jdk1.6.0_12\bin\jar -xf ../AcaciaLex.jar
 
