92871e98a67a9ec638467f1400eeca6bf102551d
manacacia@gmail.com <manacacia@gmail.com@1bba4816-cec9-a7a2-e19b-818e2af6ccd6>
2014-07-02 09:42:39 +0000

    Added SQL lexer

---
+++ trunk/Source/Acacia/test/test/common/RegexTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2014 Acacia Man
+ * The program is distributed under the terms of the GNU General Public License
+ *
+ * This file is part of acacia-lex.
+ *
+ * acacia-lex is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * acacia-lex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with acacia-lex.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package test.common;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class RegexTest {
+    
+    public RegexTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
+    @Test
+    public void comments() {
+    
+          final String EXAMPLE_TEST = "/*   */ -- something";
+
+      Pattern pattern = Pattern.compile("/\\*|\\*/|--|[\\r\\n]+");
+    // In case you would like to ignore case sensitivity you could use this
+    // statement
+    // Pattern pattern = Pattern.compile("\\s+", Pattern.CASE_INSENSITIVE);
+    Matcher matcher = pattern.matcher(EXAMPLE_TEST);
+    // Check all occurance
+    while (matcher.find()) {
+      System.out.print("Start index: " + matcher.start());
+      System.out.print(" End index: " + matcher.end() + " ");
+      System.out.println(matcher.group());
+    }
+
+    assertTrue(true);
+}
+
+}
