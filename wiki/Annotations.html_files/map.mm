d59f431cb7dd9bc5fc48822273f95b3b85b0c1eb
manacacia@gmail.com <manacacia@gmail.com@1bba4816-cec9-a7a2-e19b-818e2af6ccd6>
2014-10-10 13:24:27 +0000

    Free mind annotation

---
+++ wiki/Annotations.html_files/map.mm
@@ -0,0 +1,8 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1412945098107" ID="ID_563609846" MODIFIED="1412945107338" TEXT="Annotations">
+<node CREATED="1412945542249" ID="ID_1678864136" MODIFIED="1412945546539" POSITION="right" TEXT="Docs">
+<node CREATED="1412945549229" ID="ID_905170503" MODIFIED="1412945555989" TEXT="First doc"/>
+</node>
+</node>
+</map>
