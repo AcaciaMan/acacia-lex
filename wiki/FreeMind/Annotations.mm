ebdfa877478cc5ea3478076ae84c8401e8826efc
manacacia@gmail.com <manacacia@gmail.com@1bba4816-cec9-a7a2-e19b-818e2af6ccd6>
2014-10-22 15:40:59 +0000

    Added FreeMind map description for Annotations

--- wiki/FreeMind/Annotations.mm
+++ wiki/FreeMind/Annotations.mm
@@ -1,10 +1,17 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1412945098107" ID="ID_563609846" MODIFIED="1412945107338" TEXT="Annotations">
-<node CREATED="1412945542249" ID="ID_1678864136" MODIFIED="1412945546539" POSITION="right" TEXT="Docs">
-<node CREATED="1412945549229" ID="ID_905170503" MODIFIED="1412945555989" TEXT="First doc"/>
+<node CREATED="1412945542249" ID="ID_1678864136" MODIFIED="1413991588919" POSITION="right" TEXT="Docs">
+<node CREATED="1413991549412" ID="ID_1774884784" MODIFIED="1413991564142" TEXT="Source -&gt; Browse -&gt; svn -&gt; doc"/>
+<node CREATED="1413991590109" MODIFIED="1413991590109" TEXT="AnnotationProcessing_Eclipse - acris - How to setup annotation processors with eclipse - Web framework for building applications, web sites and portals using Google Web Toolkit - Google Project Hosting.htm"/>
+<node CREATED="1413991590119" MODIFIED="1413991590119" TEXT="Code Generation using Annotation Processors in the Java language &#x2013; part 2 Annotation Processors dr. macphail&apos;s trance.htm"/>
+<node CREATED="1413991590169" MODIFIED="1413991590169" TEXT="Lexer.java"/>
+<node CREATED="1413991590169" MODIFIED="1413991590169" TEXT="Writing a Lexer in Java 1.7 using Regex Named Capturing Groups.htm"/>
 </node>
-<node CREATED="1413180299383" ID="ID_1583330265" MODIFIED="1413180305233" POSITION="left" TEXT="Declaration"/>
+<node CREATED="1413180299383" ID="ID_1583330265" MODIFIED="1413180305233" POSITION="left" TEXT="Declaration">
+<node CREATED="1413991216153" ID="ID_1492722610" MODIFIED="1413991225893" TEXT="Project AcaciaAnn"/>
+<node CREATED="1413991421575" ID="ID_1448579381" MODIFIED="1413991421575" TEXT="AnnState.java&#xa;AnnToken.java&#xa;AnnTokens.java&#xa;AnnUnknown.java&#xa;AnnLexer.java&#xa;AnnStartState.java"/>
+</node>
 <node CREATED="1413180366723" ID="ID_1703474036" MODIFIED="1413180369223" POSITION="right" TEXT="Lexer">
 <node CREATED="1413180330243" ID="ID_1910195313" MODIFIED="1413180358203" STYLE="fork" TEXT="Interface"/>
 <node CREATED="1413180307603" ID="ID_1459831976" MODIFIED="1413180376613" TEXT="Implementation"/>
